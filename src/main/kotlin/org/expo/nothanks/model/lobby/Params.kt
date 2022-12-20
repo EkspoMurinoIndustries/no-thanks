9bc74ad29ddb8165ce04648754533ec5ad332127
Kozyulin Dmitriy <d.kozyulin@crpt.ru>
2022-12-21 00:18:05 +0530

    NT-22: Scroll bar display fix

--- src/main/kotlin/org/expo/nothanks/model/lobby/Params.kt
+++ src/main/kotlin/org/expo/nothanks/model/lobby/Params.kt
@@ -1,9 +1,9 @@
 package org.expo.nothanks.model.lobby
 
 data class Params(
-    var defaultCoinsCount: Int = 2,
+    var defaultCoinsCount: Int = 9,
     var minCard: Int = 3,
     var maxCard: Int = 35,
     var extraCards: Int = 5,
-    var maxPlayerNumber: Int = 10
+    var maxPlayerNumber: Int = 7
 )
