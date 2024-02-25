c6bb4e68a50bc523a78736b6204965e09c5b328b
Torgash <Lesha2134@gmail.com>
2024-02-25 09:47:15 +0400

    descending result sort changed to ascending

--- src/main/resources/static/js/render.js
+++ src/main/resources/static/js/render.js
@@ -248,7 +248,7 @@ function renderEndRoundScreen(results) {
     gameScreen.hide()
     $('#result-table').html('')
     $('#result-screen').show()
-    Object.values(results).sort((a, b) => b.totalScore - a.totalScore).forEach(addResultRow)
+    Object.values(results).sort((a, b) => a.totalScore - b.totalScore).forEach(addResultRow)
 }
 
 function addResultRow(playerResultValue) {
